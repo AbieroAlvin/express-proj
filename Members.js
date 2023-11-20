366a0ddc7026df60358d0c63259b8770d9837a5b
[AbieroAlvin] <[alvinabiero@gmail.com]>
2023-11-20 21:20:58 +0300

    first commit

---
+++ Members.js
@@ -0,0 +1,22 @@
+const members = [
+  {
+    id: 1,
+    name: "John Doe",
+    email: "johndoe@gmail.com",
+    status: "active",
+  },
+  {
+    id: 2,
+    name: "Alvin Johns",
+    email: "alvin@gmail.com",
+    status: "inactive",
+  },
+  {
+    id: 3,
+    name: "Fay Jay",
+    email: "fay@gmail.com",
+    status: "active",
+  },
+];
+
+module.exports = members;
